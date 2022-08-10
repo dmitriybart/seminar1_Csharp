773f6eaeaff70a270ea5f273f1a6b53034a00eea
Dmitriy B <kupperfeet@gmail.com>
2022-08-10 17:21:16 +0300

    task1 done

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+double x = int.Parse(Console.ReadLine());
+Console.Write("Квадрат Вашего числа: ");
+Console.WriteLine(x*x);
