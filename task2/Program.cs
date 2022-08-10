c53c32ccf2ec4b490295111b933ebfc56240b1f9
Dmitriy B <kupperfeet@gmail.com>
2022-08-10 17:53:27 +0300

    task2 done2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Введите первое число: ");
 double x = double.Parse(Console.ReadLine());
-Console.Write("ВВедите второе число: ");
+Console.Write("Введите второе число: ");
 double y = double.Parse(Console.ReadLine());
 if (x > y)
 {
@@ -9,10 +9,14 @@ if (x > y)
     Console.Write("min= ");
     Console.WriteLine(y);
 }
-else 
+else if (x < y)
 {
     Console.Write("max= ");
     Console.WriteLine(y);
     Console.Write("min= ");
     Console.WriteLine(x);
+}
+else
+{
+    Console.WriteLine("Первое число: " + x + " и второе число: " + y + " - равны.");
 }
