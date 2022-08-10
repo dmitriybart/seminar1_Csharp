664f0b522c7c40673f0a817fcf7201725affe09b
Dmitriy B <kupperfeet@gmail.com>
2022-08-10 18:39:18 +0300

    task3 done

---
+++ task3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите конечное число: ");
+int n = int.Parse (Console.ReadLine());
+int i = 1;
+while (i < n+1)
+{
+    if(i%2==0)
+    {
+        Console.Write(i + " ");
+    }
+    i++;
+}
